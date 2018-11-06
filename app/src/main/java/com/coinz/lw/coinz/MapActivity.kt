876deffdd29179b512a1855eebb4f06dc88d43ec
Lasse <wolter.lasse@gmail.com>
2018-11-06 20:00:24 +0000

    added markers for geoJson features + added network async task to mapActivity + added markers for all features of specific geoJson that was downloaded

--- app/src/main/java/com/coinz/lw/coinz/MapActivity.kt
+++ app/src/main/java/com/coinz/lw/coinz/MapActivity.kt
@@ -1,61 +1,91 @@
 package com.coinz.lw.coinz
 
 import android.location.Location
+import android.os.AsyncTask
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.os.PersistableBundle
+import android.util.Log
 import com.mapbox.android.core.location.LocationEngine
 import com.mapbox.android.core.location.LocationEngineListener
 import com.mapbox.android.core.location.LocationEnginePriority
 import com.mapbox.android.core.location.LocationEngineProvider
 import com.mapbox.android.core.permissions.PermissionsListener
 import com.mapbox.android.core.permissions.PermissionsManager
+import com.mapbox.geojson.FeatureCollection
+import com.mapbox.geojson.Point
 import com.mapbox.mapboxsdk.Mapbox
-import com.mapbox.mapboxsdk.camera.CameraUpdate
+import com.mapbox.mapboxsdk.annotations.MarkerOptions
 import com.mapbox.mapboxsdk.camera.CameraUpdateFactory
 import com.mapbox.mapboxsdk.geometry.LatLng
 import com.mapbox.mapboxsdk.location.modes.CameraMode
 import com.mapbox.mapboxsdk.location.modes.RenderMode
 import com.mapbox.mapboxsdk.maps.MapView
 import com.mapbox.mapboxsdk.maps.MapboxMap
+import com.mapbox.mapboxsdk.maps.OnMapReadyCallback
+import java.io.IOException
+import java.io.InputStream
+import java.net.HttpURLConnection
+import java.net.MalformedURLException
+import java.net.URL
 
-class MapActivity : AppCompatActivity(), PermissionsListener, LocationEngineListener{
+class MapActivity : AppCompatActivity(), OnMapReadyCallback, PermissionsListener,
+        LocationEngineListener {
+
+    private val tag = "MAP_ACTIVITY"
     private lateinit var mapView: MapView
     private lateinit var map: MapboxMap
+
     private lateinit var permissionManager: PermissionsManager
     private lateinit var originLocation: Location
 
-
     private var locationEngine: LocationEngine? = null
-
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_map)
 
         Mapbox.getInstance(this, getString(R.string.mapbox_access_token))
-        setContentView(R.layout.activity_map)
         mapView = findViewById(R.id.map_view)
         mapView.onCreate(savedInstanceState)
-        mapView.getMapAsync { mapboxMap ->
+        mapView.getMapAsync(this)
+    }
+
+    override fun onMapReady(mapboxMap: MapboxMap?) {
+        if (mapboxMap == null) {
+            Log.d(tag, "[onMapReady] the returned map is null")
+        } else {
             map = mapboxMap
+            // Set user interface options
+            map.uiSettings.isZoomControlsEnabled = true
+
+            // Make location information available
             enableLocation()
         }
     }
 
-    fun enableLocation() {
+    private fun enableLocation() {
         if (PermissionsManager.areLocationPermissionsGranted(this)) {
+            Log.d(tag, "[enableLocation] Permission to Location are granted")
             initializeLocationEngine()
             initializeLocationComponent()
+            downloadJson()
         } else {
+            Log.d(tag, "[enableLocation] Permission to Location are not granted")
             permissionManager = PermissionsManager(this)
             permissionManager.requestLocationPermissions(this)
         }
     }
 
-    @SuppressWarnings("MissingPermission")  // Permission is already checked at this point
+    @SuppressWarnings("MissingPermission")  // Permission is already checked in enableLocation()
     private fun initializeLocationEngine() {
         locationEngine = LocationEngineProvider(this).obtainBestLocationEngineAvailable()
-        locationEngine?.priority = LocationEnginePriority.HIGH_ACCURACY
+        locationEngine?.apply {
+            priority = LocationEnginePriority.HIGH_ACCURACY
+            interval = 5000 // check preferable every 5 seconds
+            fastestInterval = 1000 // at most every second
+            activate()
+        }
         locationEngine?.activate()
 
         val lastLocation = locationEngine?.lastLocation
@@ -68,7 +98,7 @@ class MapActivity : AppCompatActivity(), PermissionsListener, LocationEngineList
 
     }
 
-    @SuppressWarnings("MissingPermission")  // Permission is already checked at this point
+    @SuppressWarnings("MissingPermission")  // Permission is already checked in enableLocation()
     private fun initializeLocationComponent() {
         val locationComponent = map.locationComponent
 
@@ -78,16 +108,46 @@ class MapActivity : AppCompatActivity(), PermissionsListener, LocationEngineList
         locationComponent?.renderMode = RenderMode.NORMAL
     }
 
+    private fun downloadJson() {
+        val url_str = "http://homepages.inf.ed.ac.uk/stg/coinz/2018/10/03/coinzmap.geojson"
+        DownloadFileTask(DownloadCompleteRunner).execute(url_str)
+    }
+
+    private fun addMarkersFromGeoJson() {
+        try {
+            val jsonStr = MapActivity.DownloadCompleteRunner.result
+            val featureCollection = FeatureCollection.fromJson(jsonStr)
+            var features= featureCollection.features()
+
+            if (features == null) {
+                Log.d(tag, "[addMarkersFromGeoJson] features is null")
+            } else {
+                for (feature in features) {
+                    val point = feature.geometry() as Point
+                    val coordinates = point.coordinates()
+                    val pos = LatLng(coordinates[1], coordinates[0])
+                    map.addMarker(MarkerOptions().position(pos))
+
+                }
+            }
+
+        } catch (exception: MalformedURLException) {
+            Log.d(tag, exception.toString())
+        }
+    }
+
     private fun setCameraPosition(location: Location) {
         map.animateCamera(CameraUpdateFactory.newLatLngZoom(
                 LatLng(location.latitude, location.longitude), 16.0))
     }
 
     override fun onExplanationNeeded(permissionsToExplain: MutableList<String>?) {
+        Log.d(tag,"[onExplanationNeeded] Permissions: $permissionsToExplain")
         // Present toast or dialog to explain why the need to grant permission
     }
 
     override fun onPermissionResult(granted: Boolean) {
+        Log.d(tag, "[onPermissionResult] granted == $granted")
         if(granted) {
             enableLocation()
         }
@@ -99,20 +159,25 @@ class MapActivity : AppCompatActivity(), PermissionsListener, LocationEngineList
     }
 
     override fun onLocationChanged(location: Location?) {
-        location.let {
+        location?.let {
             originLocation = location!!
             setCameraPosition(location)
         }
     }
 
-    @SuppressWarnings("MissingPermission")  // Permission is already checked at this point
+    @SuppressWarnings("MissingPermission")  // Permission is already checked in enableLocation()
     override fun onConnected() {
+        Log.d(tag, "[onConnected] requesting location updates")
         locationEngine?.requestLocationUpdates()
     }
 
+    @SuppressWarnings("MissingPermission")  // Permission is already checked in enableLocation()
     override fun onStart() {
         super.onStart()
         mapView.onStart()
+        if (PermissionsManager.areLocationPermissionsGranted(this)) {
+            locationEngine?.requestLocationUpdates()
+        }
     }
 
     override fun onResume() {
@@ -125,13 +190,17 @@ class MapActivity : AppCompatActivity(), PermissionsListener, LocationEngineList
         mapView.onPause()
     }
 
+    @SuppressWarnings("MissingPermission")  // Permission is already checked in enableLocation()
     override fun onStop() {
         super.onStop()
+        locationEngine?.removeLocationUpdates()
         mapView.onStop()
     }
 
+    @SuppressWarnings("MissingPermission")  // Permission is already checked in enableLocation()
     override fun onDestroy() {
         super.onDestroy()
+        locationEngine?.deactivate()
         mapView.onDestroy()
     }
 
@@ -146,5 +215,53 @@ class MapActivity : AppCompatActivity(), PermissionsListener, LocationEngineList
         super.onLowMemory()
         mapView.onLowMemory()
     }
+
+    // Classes needed for network download
+    interface DownloadCompleteListener {
+        fun downloadComplete(result: String)
+    }
+
+    object DownloadCompleteRunner : DownloadCompleteListener {
+        var result: String = ""
+        override fun downloadComplete(result: String) {
+            this.result = result
+        }
+    }
+
+    inner class DownloadFileTask(private val caller : DownloadCompleteListener) :
+            AsyncTask<String, Void, String>() {
+
+        override fun doInBackground(vararg urls: String): String = try {
+            loadFileFromNetwork(urls[0])
+        } catch (e: IOException) {
+            "Unable to load content. Check your network connection"
+        }
+
+        private fun loadFileFromNetwork(urlString: String): String {
+            val stream: InputStream = downloadUrl(urlString)
+            return stream.bufferedReader().use { it.readText() }
+
+        }
+
+        // Given a string representation of a URL, sets up a connection and gets an input stream
+        @Throws(IOException::class)
+        private fun downloadUrl(urlString: String): InputStream {
+            val url = URL(urlString)
+            val conn = url.openConnection() as HttpURLConnection
+            conn.readTimeout = 10000 // milliseconds
+            conn.connectTimeout = 15000 // milliseconds
+            conn.requestMethod = "GET"
+            conn.doInput = true
+            conn.connect()  // starts the query
+            return conn.inputStream
+        }
+
+        override fun onPostExecute(result: String) {
+            super.onPostExecute(result)
+
+            caller.downloadComplete(result)
+            addMarkersFromGeoJson()
+        }
+    } // end class DownloadFileTask
 
 }
